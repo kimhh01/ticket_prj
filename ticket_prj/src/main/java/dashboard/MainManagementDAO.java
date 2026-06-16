93e290230bd86b6ea4dc89543dbdebef9a31b9f3
user <user@DESKTOP-E807RBG>
2026-06-16 17:50:53 +0900

    260616/박영훈/dashboard - MainManagementDAO 추가

---
+++ ticket_prj/src/main/java/dashboard/MainManagementDAO.java
@@ -0,0 +1,33 @@
+package dashboard;
+
+public class MainManagementDAO {
+	
+	public int selectTotalBooking() {
+		return 0;
+	}//selectTotalBooking
+	
+	public int selectBookingtrend() {
+		return 0;
+	}//selectBookingtrend
+	
+	public int selectTotalMember() {
+		return 0;
+	}//selectTotalMember
+	
+	public int selectMemberTrend() {
+		return 0;
+	}//selectMemberTrend
+	
+	public int selectTotalInquiry() {
+		return 0;
+	}//selectTotalInquiry
+	
+	public int selectInquiryTrend() {
+		return 0;
+	}//selectInquiryTrend
+	
+	public int selectTotalRevenue() {
+		return 0;
+	}//selectTotalRevenue
+	
+}
