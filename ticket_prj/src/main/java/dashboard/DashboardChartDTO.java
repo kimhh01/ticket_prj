08a38db4707cdaff8b7628e95d22e39c6e475ae4
Almade <Almade@DESKTOP-E2FJVL6>
2026-06-18 01:33:39 +0900

    260618 / 박영훈 / dashboard - 대시보드 나머지 추가

---
+++ ticket_prj/src/main/java/dashboard/DashboardChartDTO.java
@@ -0,0 +1,11 @@
+package dashboard;
+
+public class DashboardChartDTO {
+	
+	private List<MonthlyChartDTO> monthlyChartData;
+    private int bookingCount;
+    private int cancelCount;
+    private int totalCount;
+    private List<DailyChartDTO> dailyChartData;
+    
+}//class
