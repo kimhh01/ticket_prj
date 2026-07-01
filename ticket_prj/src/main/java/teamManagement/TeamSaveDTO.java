68a0526cb0663b39a3dd769cf6e6964dc778022e
kimhh01 <12345kimm@naver.com>
2026-07-01 21:57:40 +0900

    Delete ticket_prj/src/main/java/teamManagement directory
    Signed-off-by: kimhh01 <12345kimm@naver.com>

--- ticket_prj/src/main/java/teamManagement/TeamSaveDTO.java
+++
@@ -1,9 +0,0 @@
-package teamManagement;
-
-public class TeamSaveDTO {
-	private int teamCode;
-	private String teamName;
-	private String teamShortName;
-	private int stadiumCode;
-	private String teamLogoImg;
-}
