36875ea2f07a14193c087ba68ad34d2ff02fc888
kimhh01 <12345kimm@naver.com>
2026-07-09 22:59:02 +0900

    크롤링 파일 수정
    Signed-off-by: kimhh01 <12345kimm@naver.com>

--- ticket_prj/crawler/run_kbo_record.bat
+++ ticket_prj/crawler/run_kbo_record.bat
@@ -1,5 +1,8 @@
 @echo off
+chcp 65001 > nul
 
-cd /d C:\dev\workspace\ticket_copy_prj\crawler
+cd /d C:\ticket_prj\crawler
 
-python kbo_record_job.py >> kbo_record_log.txt 2>&1
+C:\Users\user\AppData\Local\Python\pythoncore-3.14-64\python.exe "C:\ticket_prj\crawler\python kbo_record_job.py" >> kbo_record_log.txt 2>&1
+
+exit /b %ERRORLEVEL%
